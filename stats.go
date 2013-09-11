37b1154e306052768a6d24e12a98bab6d2f9751d
Naitik Shah <n@daaku.org>
2013-09-11 16:50:55 -0700

    remove notion of "default backend"

--- stats.go
+++ stats.go
@@ -2,50 +2,8 @@
 // to add stats without locking into a specific backend.
 package stats
 
-import (
-	"flag"
-	"log"
-)
-
-// Defines the backend implementation.
+// The abstract API provided by a backend.
 type Backend interface {
 	Count(name string, count int)
 	Record(name string, value float64)
-}
-
-var (
-	verbose = flag.Bool("stats.verbose", false, "Enable verbose logging.")
-	backend Backend
-)
-
-// Set the stats backend.
-func SetBackend(b Backend) {
-	backend = b
-}
-
-// Record a value.
-func Record(name string, value float64) {
-	if *verbose {
-		log.Printf("stats.Value(%s, %f)", name, value)
-	}
-	if backend == nil {
-		return
-	}
-	backend.Record(name, value)
-}
-
-// Increment counter by given value.
-func Count(name string, count int) {
-	if *verbose {
-		log.Printf("stats.Count(%s, %d)", name, count)
-	}
-	if backend == nil {
-		return
-	}
-	backend.Count(name, count)
-}
-
-// Increment counter by 1.
-func Inc(name string) {
-	Count(name, 1)
 }
